f96eb2f31a239f8032cc26a280e35a429b1098fe
haoge <934622645@qq.com>
2021-05-23 10:10:15 +0800

    vector -- uniquify dissordered

--- Calculate/Vector.h
+++ Calculate/Vector.h
@@ -8,14 +8,15 @@
 #define START_VECTOR_H
 
 using namespace std;
-using Rank = int; //秩
-#define DEFAULT_CAPACITY  3 //默认的初始容量（实际应用中可设置为更大）
+using Rank = int;          //秩
+#define DEFAULT_CAPACITY 3 //默认的初始容量（实际应用中可设置为更大）
 
 #include <vector>
 #include <iostream>
 
-template<typename T>
-class Vector { //向量模板类
+template <typename T>
+class Vector
+{ //向量模板类
 protected:
     Rank _size;
     int _capacity;
@@ -26,22 +27,23 @@ protected:
     void expand(); //空间不足时扩容
     // O(1)-O(1) - share delete space
     void shrink(); //装填因子过小时压缩
-//    bool bubble ( Rank lo, Rank hi ); //扫描交换
-//    void bubbleSort ( Rank lo, Rank hi ); //起泡排序算法
-//    Rank maxItem ( Rank lo, Rank hi ); //选取最大元素
-//    void selectionSort ( Rank lo, Rank hi ); //选择排序算法
-//    void merge ( Rank lo, Rank mi, Rank hi ); //归并算法
-//    void mergeSort ( Rank lo, Rank hi ); //归并排序算法
-//    void heapSort ( Rank lo, Rank hi ); //堆排序（稍后结合完全堆讲解）
-//    Rank partition ( Rank lo, Rank hi ); //轴点构造算法
-//    void quickSort ( Rank lo, Rank hi ); //快速排序算法
-//    void shellSort ( Rank lo, Rank hi ); //希尔排序算法
+    //    bool bubble ( Rank lo, Rank hi ); //扫描交换
+    //    void bubbleSort ( Rank lo, Rank hi ); //起泡排序算法
+    //    Rank maxItem ( Rank lo, Rank hi ); //选取最大元素
+    //    void selectionSort ( Rank lo, Rank hi ); //选择排序算法
+    //    void merge ( Rank lo, Rank mi, Rank hi ); //归并算法
+    //    void mergeSort ( Rank lo, Rank hi ); //归并排序算法
+    //    void heapSort ( Rank lo, Rank hi ); //堆排序（稍后结合完全堆讲解）
+    //    Rank partition ( Rank lo, Rank hi ); //轴点构造算法
+    //    void quickSort ( Rank lo, Rank hi ); //快速排序算法
+    //    void shellSort ( Rank lo, Rank hi ); //希尔排序算法
 public:
     // O(n)-O(n) 构造函数
     explicit Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0) //容量为c、规模为s、所有元素初始为v
     {
         _elem = new T[_capacity = c];
-        for (_size = 0; _size < s; _elem[_size++] = v);
+        for (_size = 0; _size < s; _elem[_size++] = v)
+            ;
     } //s<=c NOLINT(bugprone-suspicious-semicolon)
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cppcoreguidelines-pro-type-member-init"
@@ -63,20 +65,24 @@ public:
     void print();
 
     // 只读访问接口
-    Rank size() const { return _size; } //规模
+    Rank size() const { return _size; }   //规模
     bool empty() const { return !_size; } //判空
+    // O(n)-O(1)
+    int disordered() const;
     // O(n)-O(1)
     // return : -1--error
     Rank find(T const &e) const { return find(e, 0, _size); } //无序向量整体查找
     // O(n)-O(1)
     // return : -1--error
     Rank find(T const &e, Rank lo, Rank hi) const; //无序向量区间查找
-//    Rank search ( T const& e ) const //有序向量整体查找
-//    { return ( 0 >= _size ) ? -1 : search ( e, 0, _size ); }
-//    Rank search ( T const& e, Rank lo, Rank hi ) const; //有序向量区间查找
-//    // 可写访问接口
+
+    //    Rank search ( T const& e ) const //有序向量整体查找
+    //    { return ( 0 >= _size ) ? -1 : search ( e, 0, _size ); }
+    //    Rank search ( T const& e, Rank lo, Rank hi ) const; //有序向量区间查找
+    //    // 可写访问接口
     T &operator[](Rank r); //重载下标操作符，可以类似于数组形式引用各元素
-//    const T& operator[] ( Rank r ) const; //仅限于做右值的重载版本
+
+    //    const T& operator[] ( Rank r ) const; //仅限于做右值的重载版本
 
     // O(n)-O(n)
     Vector<T> &operator=(Vector<T> const &); //重载赋值操作符，以便直接克隆向量
@@ -90,157 +96,212 @@ public:
     Rank insert(Rank r, T const &e); //插入元素
     // O(n)-O(1)
     Rank insert(T const &e) { return insert(_size, e); } //默认作为末元素插入
-//    void sort ( Rank lo, Rank hi ); //对[lo, hi)排序
-//    void sort() { sort ( 0, _size ); } //整体排序
+
+    //    void sort ( Rank lo, Rank hi ); //对[lo, hi)排序
+    //    void sort() { sort ( 0, _size ); } //整体排序
     // O(n)-O(1)
     void unsort(Rank lo, Rank hi); //对[lo, hi)置乱
     // O(n)-O(1)
     void unsort() { unsort(0, _size); } //整体置乱
     // O(n*n)-O(1)
     int deduplicate(); //无序去重
-//    int uniquify(); //有序去重
+    // O(n)-O(1)
+    int uniquify();    //有序去重
     // 遍历
 
     // O(n)-O(1)
-    void traverse ( void (* ) ( T& ) ); //遍历（使用函数指针，只读或局部性修改）
+    void traverse(void (*)(T &)); //遍历（使用函数指针，只读或局部性修改）
     // O(n)-O(1)
-    template <typename VST> void traverse ( VST& ); //遍历（使用函数对象，可全局性修改）
+    template <typename VST>
+    void traverse(VST &); //遍历（使用函数对象，可全局性修改）
 };
 
-template<typename T>
-void Vector<T>::copyFrom(const T *A, Rank lo, Rank hi) {
+template <typename T>
+void Vector<T>::copyFrom(const T *A, Rank lo, Rank hi)
+{
     _elem = new T[_capacity = 2 * (hi - lo)];
     _size = 0;
-    while (lo < hi) _elem[_size++] = A[lo++];
+    while (lo < hi)
+        _elem[_size++] = A[lo++];
 }
 
-template<typename T>
-Vector<T> &Vector<T>::operator=(Vector<T> const &v) {
-    if (&v == this) return *this;
-    if (_elem) delete[] _elem;
+template <typename T>
+Vector<T> &Vector<T>::operator=(Vector<T> const &v)
+{
+    if (&v == this)
+        return *this;
+    if (_elem)
+        delete[] _elem;
     copyFrom(v._elem, 0, v._size);
     return *this;
 }
 
-template<typename T>
-void Vector<T>::expand() {
-    if (_size < _capacity) return;
-    if (_capacity < DEFAULT_CAPACITY) _capacity = DEFAULT_CAPACITY;
+template <typename T>
+void Vector<T>::expand()
+{
+    if (_size < _capacity)
+        return;
+    if (_capacity < DEFAULT_CAPACITY)
+        _capacity = DEFAULT_CAPACITY;
     T *oleElem = _elem;
     _elem = new T[_capacity <<= 1];
-    for (int i = 0; i < _size; ++i) {
+    for (int i = 0; i < _size; ++i)
+    {
         _elem[i] = oleElem[i];
     }
     delete[] oleElem;
 }
 
-template<typename T>
-void Vector<T>::shrink() {
-    if (_capacity < DEFAULT_CAPACITY << 1) return;
-    if (_size << 2 > _capacity) return;
+template <typename T>
+void Vector<T>::shrink()
+{
+    if (_capacity < DEFAULT_CAPACITY << 1)
+        return;
+    if (_size << 2 > _capacity)
+        return;
     T *oleElem = _elem;
     _elem = new T[_capacity >>= 1];
-    for (int i = 0; i < _size; ++i) {
+    for (int i = 0; i < _size; ++i)
+    {
         _elem[i] = oleElem[i];
     }
     delete[] oleElem;
 }
 
-template<typename T>
-void Vector<T>::test() {
+template <typename T>
+void Vector<T>::test()
+{
     this->_size = 3;
     this->expand();
     this->_size = 1;
     this->shrink();
 }
 
-template<typename T>
-T &Vector<T>::operator[](Rank r) {
+template <typename T>
+T &Vector<T>::operator[](Rank r)
+{
     return _elem[r];
 }
 
-template<typename T>
-void Vector<T>::print() {
-    for (int i = 0; i < _size; ++i) std::cout << _elem[i] << '\t';
+template <typename T>
+void Vector<T>::print()
+{
+    for (int i = 0; i < _size; ++i)
+        std::cout << _elem[i] << '\t';
     std::cout << '\n';
 }
 
-template<typename T>
-void Vector<T>::unsort(Rank lo, Rank hi) {
-    srand((unsigned int) time(nullptr)); // NOLINT(cert-msc51-cpp)
+template <typename T>
+void Vector<T>::unsort(Rank lo, Rank hi)
+{
+    srand((unsigned int)time(nullptr)); // NOLINT(cert-msc51-cpp)
     T *V = _elem + lo;
-    for (Rank i = hi - lo; i > 0; i--) swap(V[i - 1], V[rand() % i]); // NOLINT(cert-msc50-cpp)
+    for (Rank i = hi - lo; i > 0; i--)
+        swap(V[i - 1], V[rand() % i]); // NOLINT(cert-msc50-cpp)
 }
 
-template<typename T>
-Rank Vector<T>::find(const T &e, Rank lo, Rank hi) const {
-    while ((lo < hi--) && (_elem[hi] != e));
+template <typename T>
+Rank Vector<T>::find(const T &e, Rank lo, Rank hi) const
+{
+    while ((lo < hi--) && (_elem[hi] != e))
+        ;
     return hi;
 }
 
-template<typename T>
-Rank Vector<T>::insert(Rank r, const T &e) {
+template <typename T>
+Rank Vector<T>::insert(Rank r, const T &e)
+{
     expand();
-    for (int i = _size; i > r; i--) _elem[i] = _elem[i - 1];
+    for (int i = _size; i > r; i--)
+        _elem[i] = _elem[i - 1];
     _elem[r] = e;
     _size++;
     return r;
 }
 
-template<typename T>
-T Vector<T>::remove(Rank r) {
+template <typename T>
+T Vector<T>::remove(Rank r)
+{
     T re = _elem[r];
     remove(r, r + 1);
     return re;
 }
 
-template<typename T>
-int Vector<T>::remove(Rank lo, Rank hi) {
-    if (lo >= hi) return 0;
-    for (; hi < _size; _elem[lo++] = _elem[hi++]);
+template <typename T>
+int Vector<T>::remove(Rank lo, Rank hi)
+{
+    if (lo >= hi)
+        return 0;
+    for (; hi < _size; _elem[lo++] = _elem[hi++])
+        ;
     _size = lo;
     shrink();
     return hi - lo;
 }
 
-template<typename T>
-int Vector<T>::deduplicate() {
+template <typename T>
+int Vector<T>::deduplicate()
+{
     int oldSize = _size;
     int i = 1;
-    while (i < _size) {
+    while (i < _size)
+    {
         (find(_elem[i], 0, i) < 0) ? i++ : remove(i);
     }
     return oldSize - _size;
 }
 
-template<typename T>
-void Vector<T>::traverse(void (*visitor)(T &)) {
-    for (int i = 0; i < _size; ++i) visitor(_elem[i]);
+template <typename T>
+int Vector<T>::uniquify()
+{
+    int oldSize = _size;
+    int i = 1;
+    while (i < _size)
+    {
+        (_elem[i-1]!= _elem[i]) ? i++ : remove(i);
+    }
+    return oldSize - _size;
 }
 
-template<typename T>
-template<typename VST>
-void Vector<T>::traverse(VST &visitor) {
-    for (int i = 0; i < _size; ++i) visitor(_elem[i]);
+template <typename T>
+void Vector<T>::traverse(void (*visitor)(T &))
+{
+    for (int i = 0; i < _size; ++i)
+        visitor(_elem[i]);
 }
 
+template <typename T>
+template <typename VST>
+void Vector<T>::traverse(VST &visitor)
+{
+    for (int i = 0; i < _size; ++i)
+        visitor(_elem[i]);
+}
 
+template <typename T>
+int Vector<T>::disordered() const
+{
+    int n = 0;
+    for (int i = 1; i < _size; ++i)
+        if (_elem[i - 1] > _elem[i])
+            n++;
+    return n;
+}
 
 //Vector
 
 // tool function
 
-template<typename T>
+template <typename T>
 static bool lt(T *a, T *b) { return lt(*a, *b); }
 
-template<typename T>
+template <typename T>
 static bool lt(T &a, T &b) { return a < b; }
 
-template<typename T>
+template <typename T>
 static bool eq(T *a, T *b) { return eq(*a, *b); }
 
-template<typename T>
+template <typename T>
 static bool eq(T &a, T &b) { return a == b; }
 
 #endif //START_VECTOR_H
-
--- Google_tests/VectorTest.cpp
+++ Google_tests/VectorTest.cpp
@@ -9,24 +9,27 @@
 #include <gtest/gtest.h>
 #include <Vector.h>
 
-class VectorTest : public testing::Test {
+class VectorTest : public testing::Test
+{
 };
 
-TEST_F(VectorTest, constructor) {
+TEST_F(VectorTest, constructor)
+{
     int l[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "UnusedValue"
-    Vector<int> a; // 默认构造
-    Vector<int> a1(l, 10); // 拷贝构造-1
-    Vector<int> a2(l, 4, 10); // 拷贝构造-2
-    Vector<int> a3(a1); // 拷贝构造-3 NOLINT(performance-unnecessary-copy-initialization)
+    Vector<int> a;             // 默认构造
+    Vector<int> a1(l, 10);     // 拷贝构造-1
+    Vector<int> a2(l, 4, 10);  // 拷贝构造-2
+    Vector<int> a3(a1);        // 拷贝构造-3 NOLINT(performance-unnecessary-copy-initialization)
     Vector<int> a4(a1, 4, 10); // 拷贝构造-4 NOLINT(performance-unnecessary-copy-initialization)
     a4 = a3;
 #pragma clang diagnostic pop
     printf("constructor test pass\n");
 }
 
-TEST_F(VectorTest, inside) {
+TEST_F(VectorTest, inside)
+{
     int l[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     Vector<int> a(l, 10);
     a.unsort();
@@ -34,11 +37,13 @@ TEST_F(VectorTest, inside) {
     printf("inside test pass\n");
 }
 
-void print(int& e){
+void print(int &e)
+{
     std::cout << e << '\t';
 }
 
-TEST_F(VectorTest, CRUD) {
+TEST_F(VectorTest, CRUD)
+{
     int l[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     Vector<int> a(l, 10);
     ASSERT_EQ(8, a.insert(8, 11));
@@ -46,9 +51,10 @@ TEST_F(VectorTest, CRUD) {
     ASSERT_EQ(11, a.remove(8));
     ASSERT_EQ(-1, a.find(11));
     ASSERT_EQ(5, a.insert(5, 3));
+    ASSERT_EQ(1, a.disordered());
     ASSERT_EQ(5, a.find(3));
     ASSERT_EQ(1, a.deduplicate());
+    ASSERT_EQ(2, a.insert(2, 3));
+    ASSERT_EQ(1,a.uniquify());
     a.traverse(print);
-
 }
-
